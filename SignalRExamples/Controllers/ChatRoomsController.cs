a3daceb1a80781b93057012e3acd35ac95d371b2
Ionuț - Cristian Cânciu <49305908+icanci01@users.noreply.github.com>
2023-12-29 16:01:21 +0200

    Add ChatRoomsController and update packages
    A new controller, ChatRoomsController, has been added for better handling of chat room data. In the .csproj file, packages have been updated and a new package (`Microsoft.EntityFrameworkCore.Sqlite`) has been introduced to improve database operations. A new version (`7.0.11`) of `Microsoft.VisualStudio.Web.CodeGeneration.Design` has also been added.

---
+++ SignalRExamples/Controllers/ChatRoomsController.cs
@@ -0,0 +1,59 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SignalRExamples.Data;
+using SignalRExamples.Models;
+
+namespace SignalRExamples.Controllers;
+
+[ApiController]
+public class ChatRoomsController : ControllerBase
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public ChatRoomsController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    [Route("api/[controller]/GetChatRooms")]
+    public async Task<ActionResult<IEnumerable<ChatRoom>>> GetChatRoom()
+    {
+        return await _dbContext.ChatRoom.ToListAsync();
+    }
+
+    [HttpGet]
+    [Route("api/[controller]/GetChatUsers")]
+    public async Task<ActionResult<IEnumerable<object>>> GetChatUser()
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var users = await _dbContext.Users.ToListAsync();
+
+        return users.Where(u => u.Id != userId).Select(u => new { u.Id, u.UserName }).ToList();
+    }
+
+    [HttpPost]
+    [Route("api/[controller]/PostChatRoom")]
+    public async Task<ActionResult<ChatRoom>> PostChatRoom(ChatRoom chatRoom)
+    {
+        _dbContext.ChatRoom.Add(chatRoom);
+        await _dbContext.SaveChangesAsync();
+
+        return CreatedAtAction("GetChatRoom", new { id = chatRoom.Id }, chatRoom);
+    }
+
+    [HttpDelete]
+    [Route("api/[controller]/DeleteChatRoom/{id}")]
+    public async Task<IActionResult> DeleteChatRoom(int id)
+    {
+        var chatRoom = await _dbContext.ChatRoom.FindAsync(id);
+
+        if (chatRoom == null) return NotFound();
+
+        _dbContext.ChatRoom.Remove(chatRoom);
+        await _dbContext.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
