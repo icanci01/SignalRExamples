0c1c9b3649423f692dabab8d7fd348d89e13e808
Ionuț - Cristian Cânciu <49305908+icanci01@users.noreply.github.com>
2024-01-01 14:58:59 +0200

    Update UI elements and improve connection status handling

--- src/SignalR.Api/wwwroot/js/site.js
+++ src/SignalR.Api/wwwroot/js/site.js
@@ -1,19 +1,19 @@
 ﻿function setStatusConnected() {
-    document.getElementById("connectionStatusText").innerText = "Connected";
+    document.getElementById("connectionHubStatusText").innerText = "Connected";
     document.getElementById("connectionStatus").style.color = "green";
 }
 
 function setStatusReconnecting() {
-    document.getElementById("connectionStatusText").innerText = "Reconnecting";
+    document.getElementById("connectionHubStatusText").innerText = "Reconnecting";
     document.getElementById("connectionStatus").style.color = "orange";
 }
 
 function setStatusDisconnected() {
-    document.getElementById("connectionStatusText").innerText = "Disconnected";
+    document.getElementById("connectionHubStatusText").innerText = "Disconnected";
     document.getElementById("connectionStatus").style.color = "red";
 }
 
 function setStatusFailed() {
-    document.getElementById("connectionStatusText").innerText = "Failed";
+    document.getElementById("connectionHubStatusText").innerText = "Failed";
     document.getElementById("connectionStatus").style.color = "red";
 }
